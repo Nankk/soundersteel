7973cad89f36302db24483122870ea60c44089da
Nankk <toc32768@gmail.com>
2020-08-04 02:08:26 +0900

    :wrench: Remove npm uuid
    （cljsに標準でuuidが含まれているとは）知らなかった

--- src/frontend/db.cljs
+++ src/frontend/db.cljs
@@ -1,4 +1,31 @@
 (ns frontend.db)
 
+(def debug-db
+  (let [f-id  (random-uuid)
+        s-id  (random-uuid)
+        s-id2 (random-uuid)
+        t-id  (random-uuid)
+        l-id  (random-uuid)]
+    {:master-volume 1
+     :files         [{:id   f-id
+                      :path "/home/user/Music/hogefuga.mp3"}]
+     :scenes        [{:id     s-id
+                      :name   "Piyo"
+                      :tracks [t-id]}
+                     {:id     s-id2
+                      :name   "Poyo"
+                      :tracks []}]
+     :tracks        [{:id      t-id
+                      :file-id f-id
+                      :name    "hoge-fugue"
+                      :loop    {:id         l-id
+                                :loop       {:begin 10
+                                             :end   60}
+                                :volume     1
+                                :wavesurfer nil}}]
+     :cur-file-id   nil
+     :cur-scene-id  nil
+     :cur-track-id  nil}))
+
 (def default-db
   {:name "re-frame"})
