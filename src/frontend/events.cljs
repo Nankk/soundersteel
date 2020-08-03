0ce5a02b112d7134df76ca5d15d6178a59becc3d
Nankk <toc32768@gmail.com>
2020-08-04 02:05:51 +0900

    :shirt: Neatify subs & events
    データは1種類にまとめ、2番目のアイテムがコレクションかどうかで分岐

--- src/frontend/events.cljs
+++ src/frontend/events.cljs
@@ -1,14 +1,19 @@
 (ns frontend.events
-  (:require
-   [re-frame.core :as rf]
-   [frontend.db :as db]))
+  (:require [frontend.config :as config]
+            [frontend.db :as db]
+            [re-frame.core :as rf]))
 
-(rf/reg-event-db
-  ::initialize-db
-  (fn [_ _]
-    (println db/default-db)
-    db/default-db))
+(def handlers
+  {::set-cur-file-id  [:cur-file-id]
+   ::set-cur-scene-id [:cur-scene-id]
+   ::set-cur-track-id [:cur-track-id]
+   ::add-scene        (fn [db [_ _]]
+                        (update db :scenes conj {:id (random-uuid) :name "New scene" :tracks []}))
+   ::initialize-db    (fn [_ _] (if config/debug? db/debug-db db/default-db))
+   ::add-ipc-channel  (fn [db [_ k v]] (update-in db [:ipc-channels] assoc k v))})
 
-(rf/reg-event-db ::add-ipc-channel
-  (fn [db [_ k v]]
-    (update-in db [:ipc-channels] assoc k v)))
+(doseq [[ev-key item] handlers]
+  (if (coll? item)
+    (rf/reg-event-db ev-key
+      (fn [db [_ v]] (assoc-in db (vec item) v)))
+    (rf/reg-event-db ev-key item)))
--- src/frontend/subs.cljs
+++ src/frontend/subs.cljs
@@ -3,11 +3,22 @@
    [re-frame.core :as rf]
    ))
 
-(rf/reg-sub
-  ::name
-  (fn [db _]
-    (db :name)))
+(def subscriptions
+  {::ipc-channels [:ipc-channels]
+   ::files        [:files]
+   ::scenes       [:scenes]
+   ::tracks       [:tracks]
+   ::cur-file-id  [:cur-file-id]
+   ::cur-scene-id (fn [db _]
+                    (println "subs/cur-scene-id")
+                    (get-in db [:cur-scene-id]))
+   ::cur-track-id [:cur-track-id]
+   ::cur-file (fn [db _] (first (filter #(= (% :id) (db :cur-file-id)) (db :files))))
+   ::cur-scene (fn [db _] (first (filter #(= (% :id) (db :cur-scene-id)) (db :scenes))))
+   ::cur-track (fn [db _] (first (filter #(= (% :id) (db :cur-track-id)) (db :tracks))))})
 
-(rf/reg-sub ::ipc-channels
-  (fn [db [_ _]]
-    (get-in db [:ipc-channels])))
+(doseq [[sub-key item] subscriptions]
+  (if (coll? item)
+    (rf/reg-sub sub-key
+      (fn [db _] (get-in db (vec item))))
+    (rf/reg-sub sub-key item)))
