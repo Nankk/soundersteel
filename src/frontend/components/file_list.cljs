76e491e450f59d92f98d820bc9e68944f22102ca
Nankk <toc32768@gmail.com>
2020-08-04 02:22:40 +0900

    :sparkles: Mock file-list

--- src/frontend/components/file_list.cljs
+++ src/frontend/components/file_list.cljs
@@ -1,4 +1,28 @@
-(ns frontend.components.file-list)
+(ns frontend.components.file-list
+  (:require
+   [frontend.subs :as subs]
+   [frontend.util :as util]
+   [re-frame.core :as rf]))
+
+(defn- file-list []
+  [:div.upper
+   [:div.grid-upper
+    [:div.title
+     [:h3 "Files"]]
+    [:div.list
+     [:div.card
+      [:div.card-body
+       (let [fs @(rf/subscribe [::subs/files])]
+         (for [f fs]
+           ^{:key f}
+           [:div
+            [:div (str "ID " (f :id))]
+            [:div (str "Path "(f :path))]]
+           ))]]]]])
 
 (defn main []
-  [:div "File list"])
+  [:div#file-list.container
+   [:div.grid
+    [file-list]
+    [:div.lower
+     [:div "File info"]]]])
--- src/frontend/style/file_list.cljs
+++ src/frontend/style/file_list.cljs
@@ -5,4 +5,25 @@
 
 (defn css []
   (g/css
-   ))
+   [:#file-list
+    ;; Global grid
+    [:.grid {:height              "100%"
+             :width               "100%"
+             :display             "grid"
+             :grid-template-rows  "1fr 80px"
+             :grid-template-areas (pr-str
+                                   "upper"
+                                   "lower")}]
+    [:.upper {:grid-area "upper"}]
+    [:.lower {:grid-area "lower"}]
+
+    ;; Inner grid
+    [:.grid-upper {:height              "100%"
+                   :width               "100%"
+                   :display             "grid"
+                   :grid-template-rows  "30px 1fr"
+                   :grid-template-areas (pr-str
+                                         "title"
+                                         "list")}]
+    [:.title {:grid-area "title"}]
+    [:.list {:grid-area "list"}]]))
