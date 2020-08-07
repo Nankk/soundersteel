6bcd4f405be3d957da46c3cb5ed8487b18f7436c
Nankk <toc32768@gmail.com>
2020-08-08 02:23:55 +0900

    :sparkles: Pause all tracks when scenes are switched

--- src/frontend/components/scene_list.cljs
+++ src/frontend/components/scene_list.cljs
@@ -10,21 +10,26 @@
 
 (defn main []
   [:div#scene-list.container
-   [:div.card.container {:style {:background-color (style/colors :gray-darker)}}
-    [:div.grid
-     [:div.title.d-flex.flex-row.justify-content-between.align-items-center
-      [:h3 "Scenes"]
-      [:i.fa.fa-plus {:on-click #(rf/dispatch-sync [::events/add-scene (const/default-scene)])}]]
-     [:div.list.scroll
-      (let [ss     @(rf/subscribe [::subs/scenes])
-            cur-id @(rf/subscribe [::subs/cur-scene-id])]
-        (for [s ss]
-          ^{:key s}
-          [:div.card {:style {:background-color (if (= (s :id) cur-id)
-                                                  (style/colors :blue)
-                                                  (style/colors :gray-darker))}}
-           [:div.card-body {:on-click #(rf/dispatch-sync [::events/set-cur-scene-id (s :id)])}
-            [:div.d-flex.flex-row.justify-content-between
-             [:div
-              (str (s :name))]
-             [:i.fa.fa-trash {:on-click #(rf/dispatch-sync [::events/remove-scene s])}]]]]))]]]])
+   [:div.h-100 {:style {:padding "14px"
+                        :padding-bottom "0px"}}
+    [:div.card.container
+     [:div.grid
+      [:div.title.d-flex.flex-row.justify-content-between.align-items-center
+       [:h3 "Scenes"]
+       [:i.fa.fa-plus {:on-click #(rf/dispatch-sync [::events/add-scene (const/default-scene)])}]]
+      [:div.list.scroll
+       (let [ss     @(rf/subscribe [::subs/scenes])
+             cur-id @(rf/subscribe [::subs/cur-scene-id])]
+         (for [s ss]
+           ^{:key s}
+           [:div.card {:class (if (= (s :id) cur-id) "selected" "list-item")}
+            [:div.card-body {:on-click (fn []
+                                         (when (not= (s :id) cur-id)
+                                           (doseq [t @(rf/subscribe [::subs/tracks])]
+                                             (.pause (t :wavesurfer))
+                                             (rf/dispatch-sync [::events/update-playing? t]))
+                                           (rf/dispatch-sync [::events/set-cur-scene-id (s :id)])))}
+             [:div.d-flex.flex-row.justify-content-between
+              [:div
+               (str (s :name))]
+              [:i.fa.fa-trash {:on-click #(rf/dispatch-sync [::events/remove-scene s])}]]]]))]]]]])
